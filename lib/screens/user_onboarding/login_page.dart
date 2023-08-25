27d89e65728ca6f2be405627ae7cc862e533727f
sadid369 <sadid.jones@gmail.com>
2023-08-25 22:54:37 +0600

    updated 3rd class after

--- lib/screens/user_onboarding/login_page.dart
+++ lib/screens/user_onboarding/login_page.dart
@@ -18,88 +18,148 @@ class _LoginPageState extends State<LoginPage> {
     var mWidth = mediaQueryData.size.width;
     var mHeight = mediaQueryData.size.height;
     var mOrientaion = mediaQueryData.orientation;
+    print("MQWidth: $mWidth" + "MQHeight: $mHeight");
     return Scaffold(
       // backgroundColor: ColorConstants.greyColor,
       body: Padding(
         padding: EdgeInsets.all(8),
         child: mOrientaion == Orientation.portrait
-            ? portraitUI(mWidth)
-            : landScapeUI(mWidth),
+            ? portraitUI(mWidth, mHeight)
+            : landScapeUI(mWidth, mHeight),
       ),
     );
   }
 }
 
-Widget mainUI(double mWidth, bool isLandscape) {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      CircleAvatar(
-        backgroundColor: Colors.black,
-        radius: mWidth * 0.08,
-        child: Image.asset(
-          ImagesConstants.appLogoIcon,
-          color: Colors.grey.shade100,
-          width: mWidth * 0.075,
-          height: mWidth * 0.075,
-        ),
-      ),
-      hSpacer(mHeight: 16),
-      Text(
-        'Welcome, Back!!',
-        style: mTextStyles25(
-          mWeight: FontWeight.bold,
-        ),
-      ),
-      hSpacer(),
-      TextField(
-        decoration: myDecoration(
-          mLabel: 'Email',
-          mHint: 'Enter Your Email',
-          mPrefixIcon: Icons.email_outlined,
-        ),
-      ),
-      hSpacer(),
-      TextField(
-        obscureText: true,
-        obscuringCharacter: "*",
-        decoration: myDecoration(
-          mLabel: 'Password',
-          mHint: 'Enter Your Password',
-          mPrefixIcon: Icons.password_outlined,
-        ),
-      ),
-      hSpacer(mHeight: 16),
-      AppRoundedButton(
-        onTap: () {},
-        title: 'Login',
-      )
-    ],
-  );
+Widget mainUI(
+  double mWidth,
+  double mHeight,
+) {
+  return mHeight < 334
+      ? SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.black,
+                radius: mWidth * 0.08,
+                child: Image.asset(
+                  ImagesConstants.appLogoIcon,
+                  color: Colors.grey.shade100,
+                  width: mWidth * 0.075,
+                  height: mWidth * 0.075,
+                ),
+              ),
+              hSpacer(mHeight: 16),
+              Text(
+                'Welcome, Back!!',
+                style: mTextStyles25(
+                  mWeight: FontWeight.bold,
+                ),
+              ),
+              hSpacer(),
+              TextField(
+                decoration: myDecoration(
+                  mLabel: 'Email',
+                  mHint: 'Enter Your Email',
+                  mPrefixIcon: Icons.email_outlined,
+                ),
+              ),
+              hSpacer(),
+              TextField(
+                obscureText: true,
+                obscuringCharacter: "*",
+                decoration: myDecoration(
+                  mLabel: 'Password',
+                  mHint: 'Enter Your Password',
+                  mPrefixIcon: Icons.password_outlined,
+                ),
+              ),
+              hSpacer(mHeight: 16),
+              AppRoundedButton(
+                onTap: () {},
+                title: 'Login',
+              )
+            ],
+          ),
+        )
+      : Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.black,
+              radius: mWidth * 0.08,
+              child: Image.asset(
+                ImagesConstants.appLogoIcon,
+                color: Colors.grey.shade100,
+                width: mWidth * 0.075,
+                height: mWidth * 0.075,
+              ),
+            ),
+            hSpacer(mHeight: 16),
+            SizedBox(
+              width: mWidth * 0.3,
+              child: FittedBox(
+                fit: BoxFit.scaleDown,
+                child: Text(
+                  'Welcome, Back!!',
+                  style: mTextStyles25(
+                    mWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            hSpacer(),
+            TextField(
+              decoration: myDecoration(
+                mLabel: 'Email',
+                mHint: 'Enter Your Email',
+                mPrefixIcon: Icons.email_outlined,
+              ),
+            ),
+            hSpacer(),
+            TextField(
+              obscureText: true,
+              obscuringCharacter: "*",
+              decoration: myDecoration(
+                mLabel: 'Password',
+                mHint: 'Enter Your Password',
+                mPrefixIcon: Icons.password_outlined,
+              ),
+            ),
+            hSpacer(mHeight: 16),
+            AppRoundedButton(
+              onTap: () {},
+              title: 'Login',
+            )
+          ],
+        );
 }
 
-Widget portraitUI(double mWeight) {
-  return mainUI(mWeight, false);
+Widget portraitUI(double mWeight, double mHeight) {
+  return mainUI(mWeight, mHeight);
 }
 
-Widget landScapeUI(double mWidth) {
+Widget landScapeUI(double mWidth, double mHeight) {
   return Row(
     children: [
       Expanded(
         child: CircleAvatar(
           backgroundColor: Colors.black,
-          radius: mWidth * 0.08,
+          radius: mHeight * 0.08,
           child: Image.asset(
             ImagesConstants.appLogoIcon,
             color: Colors.grey.shade100,
-            width: mWidth * 0.075,
-            height: mWidth * 0.075,
+            width: mHeight * 0.075,
+            height: mHeight * 0.075,
           ),
         ),
       ),
       Expanded(
         child: LayoutBuilder(builder: (context, constrain) {
-          return mainUI(constrain.maxWidth, true);
+          var mWidth = constrain.maxWidth;
+          print(mWidth);
+          return mainUI(constrain.maxWidth, constrain.maxHeight);
         }),
       ),
     ],
